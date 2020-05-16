375acb6dcc0555d9f158903280bc09f07113d153
Kristof De Spiegeleer <kristof@incubaid.com>
2020-05-16 08:39:48 +0200

    gitrepo

--- src/gittools/gitrepo.cr
+++ src/gittools/gitrepo.cr
@@ -17,25 +17,64 @@ module TFWeb
     property provider = "github"
     property provider_suffix = ".com"
     property environment = "production"
+    property codedir = "~/tfweb"
+    property depth = 1
 
-    def initialize(@name = "", @path = "", @url = "", @branch = "master", @branchswitch = false, @environment = "production")
+    def initialize(@name = "", @path = "", @url = "", @branch = "master", @branchswitch = false, @environment = "production", @depth = 1)
       # TODO: check if ssh-agent loaded, if yes use git notation, otherwise html
       #   @url = "" # TODO: fill in the right url (git or http), if http no authentication
       if @path == "" && @url == ""
-        raise Exception.new("path and url are empty #{name}")
+        error "path and url are empty #{name}"
       end
-      if @path != "" && !@url
-        @url = try_read_url_from_path
+
+      #
+      if @path != ""
+        url_on_fs = try_read_url_from_path
+        #give url on fs priority
+        if url_on_fs !=""
+          @url = url
+        end
       end
+
+      #put https in front if its not done yet
       if @url != ""
         if !@url.includes?("@") && !@url.starts_with?("https://")
           @url = "https://#{@url}"
         end
-        infer_provider_account_repo()
+      else
+        error "cannot initialize git repository if url not given: #{url}"
       end
+
+      #initialize the git environment, parse the separate properties
+      parse_provider_account_repo()
+
+      if sshagent_loaded
+        @url = url_as_ssh
+      else
+        @url = url_as_https
+      end
+
+      pp @url
+      exit 0
+
+      #make sure the repository exists
+      dir_repo_ensure()
+
     end
 
-    def try_read_url_from_path
+    private def error(msg)
+      puts msg
+      exit 1
+    end 
+
+    #returns true if sshagent is loaded with at least 1 key
+    private def sshagent_loaded
+      `ssh-add -l &>/dev/null`
+      $?.success?
+    end    
+
+    #fetch the url from git config, if not exist return ''
+    private def try_read_url_from_path
       res = `cd #{@path} && git config --get remote.origin.url`
       if $?.success?
         res.chomp
@@ -44,41 +83,45 @@ module TFWeb
       end
     end
 
-    def url_as_https
+    #return the git url as https
+    private def url_as_https
       "https://#{@provider}#{@provider_suffix}/#{@account}/#{@name}"
     end
 
-    def make_ssh_url
+    #return the url in git ssh format
+    private def url_as_ssh
       "git@#{@provider}:#{@account}/#{@name}"
     end
 
     def base_dir
-      "~/tfweb/#{@environment}/"
+      "#{@codedir}/#{@environment}/"
     end
 
-    def guess_repo_dir
+    #location of the git repository
+    def dir_repo
       @path = Path["#{base_dir}/#{@provider}/#{@account}/#{name}"].expand(home: true).to_s
     end
 
-    def ensure_repo_dir
-      d = guess_repo_dir()
+    #make sure the directory exists
+    private def dir_repo_ensure
+      d = dir_repo()
       Dir.mkdir_p(d)
       d
     end
 
-    def ensure_account_dir
+    private def dir_account_ensure
       d = Path["#{base_dir}/#{@provider}/#{@account}"].expand(home: true)
       Dir.mkdir_p(d)
       d
     end
 
-    def rewrite_http_to_ssh_url
-      rewritten_url = @url # let's assume ssh is the default.
-      infer_provider_account_repo
-      make_ssh_url(@provider, @account, @reponame)
-    end
+    # def rewrite_http_to_ssh_url
+    #   rewritten_url = @url # let's assume ssh is the default.
+    #   parse_provider_account_repo()
+    #   url_as_ssh(@provider, @account, @reponame)
+    # end
 
-    private def infer_provider_account_repo
+    private def parse_provider_account_repo
       account_dir = ""
       rewritten_url = @url # let's assume ssh is the default.
       if @url.starts_with?("http")
@@ -88,7 +131,7 @@ module TFWeb
           @provider_suffix = validm.not_nil!["suffix"].to_s
           @account = validm.not_nil!["account"].to_s
           @name = validm.not_nil!["repo"].to_s
-          account_dir = ensure_account_dir()
+          account_dir = dir_account_ensure()
           @path = File.join(account_dir, @name)
         end
       else
@@ -98,25 +141,44 @@ module TFWeb
             @provider = validm.not_nil!["provider"].to_s
             @account = validm.not_nil!["account"].to_s
             @name = validm.not_nil!["repo"].to_s
-            account_dir = ensure_account_dir()
+            account_dir = dir_account_ensure()
             @path = File.join(account_dir, name)
           end
         end
       end
     end
 
-    def ensure_repo(pull = false)
-      account_dir = ensure_account_dir
-      rewritten_url = @url
-      puts "cloning into #{@path}"
-      unless Dir.exists?(@path)
-        `cd #{account_dir} && git clone #{rewritten_url} --depth=1 && cd #{@name} && git fetch`
-      end
-      if pull
-        `cd #{guess_repo_dir} && git pull`
+    def pull()
+      `cd #{dir_repo} && git pull`
+      if not $?.success?
+        raise "could not pull repo: #{dir_repo}"
       end
       if @branch && @branchswitch
-        `cd #{guess_repo_dir} && git checkout #{@branch}`
+        `cd #{dir_repo} && git checkout #{@branch}`
+      end
+    end
+
+    #will reset the repo, means create if not exists
+    #will then reset to right branch & pull all changes
+    #DANGEROUS: local changes will be overwritten
+    def reset()
+      `cd #{repo_path} && git clean -xfd && git checkout . && git checkout #{branch} && git pull`
+      if not $?.success?
+        raise "could not reset repo: #{dir_repo}"
+      end
+    end
+
+    #make sure the repository exists, if not will pull
+    def repo_ensure()
+      unless Dir.exists?(@path)
+        account_dir = dir_account_ensure()
+        puts "cloning into #{@path} (dir did not exist)"  
+        if @depth!=0
+          `cd #{account_dir} && git clone #{@urll} --depth=1 && cd #{@name} && git fetch`
+        else
+          `cd #{account_dir} && git clone #{@urll}`
+        end
+        pull()
       end
 
       File.join(account_dir, @name)
@@ -127,10 +189,11 @@ module TFWeb
     # if there is data in there, ask to commit, ask message if @autocommit is on
     # if branchname specified, check branchname is the same, if not and @branchswitch is True switch branch, otherwise error
     def pull(force = false)
-      repo_path = ensure_repo # handles the cloning, existence and the correct branch already.
+      unless Dir.exists?(@path)
+        repo_ensure() # handles the cloning, existence and the correct branch already.
+      end
       if force
-        `cd #{repo_path} && git clean -xfd && git checkout . && git checkout #{branch} && git pull`
-        $?.success?
+        reset()
       else
         `cd #{repo_path} && git pull`
         $?.success?
@@ -139,24 +202,18 @@ module TFWeb
 
     # return the branchname from the repo on the filesystem, if it doesn't exist yet do an update
     private def branch_get
+      raise "not implemented"
     end
 
     # check the local repo and compare to remote, if there is newer info remote return True, otherwise False
     def check_is_new
-      raise
+      raise "not implemented"
     end
 
-    def has_sshagent
-      `ps aux | grep -v grep | grep ssh-agent`
-      $?.success?
-    end
-
-    # reset the repo, do a checkout . -F
-    # check branchname
-    def reset
-      `cd #{@path} && git checkout . -F` # => "foo"
-      $?.success?                        # => true
-    end
+    # def has_sshagent
+    #   `ps aux | grep -v grep | grep ssh-agent`
+    #   $?.success?
+    # end
 
     # delete the repo
     def delete
@@ -165,15 +222,19 @@ module TFWeb
 
     # commit the new info, automatically do an add of all files
     def commit(msg : String)
-      `git add -u && git commit -m #{msg}`
+      `cd #{repo_path} && git add -u && git commit -m #{msg}`
+    end
+
+    #commit, pull, push
+    def commit_pull_push(msg : String)
+      `cd #{repo_path} && git add -u && git commit -m #{msg}`
+      pull()
+      push()
     end
 
     def push
-      `git push`
+      `cd #{repo_path} && git push`
     end
 
-    # commit the info, do a pull if not conflicts do a push
-    def commit_pull_push(msg : String)
-    end
   end
 end
